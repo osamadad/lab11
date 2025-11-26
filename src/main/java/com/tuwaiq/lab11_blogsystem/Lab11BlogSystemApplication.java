02316c6a02bd90c32131350f0e6042fbb7020b95
osama dad <osamadad768@gmail.com>
2025-11-26 10:09:30 +0300

    initial commit

---
+++ src/main/java/com/tuwaiq/lab11_blogsystem/Lab11BlogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.lab11_blogsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11BlogSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab11BlogSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuwaiq/lab11_blogsystem/Lab11BlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.lab11_blogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11BlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
