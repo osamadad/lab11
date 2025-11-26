adb5711b1785e5a5b1a1b6cdc1d905607e1a910f
osama dad <osamadad768@gmail.com>
2025-11-26 11:34:39 +0300

    feat: comment controller basic crud

---
+++ src/main/java/com/tuwaiq/lab11_blogsystem/Controller/UserController.java
@@ -0,0 +1,64 @@
+package com.tuwaiq.lab11_blogsystem.Controller;
+
+import Api.ApiResponse;
+import com.tuwaiq.lab11_blogsystem.Model.User;
+import com.tuwaiq.lab11_blogsystem.Service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addUser(@RequestBody @Valid User user, Errors errors){
+        if (errors.hasErrors()){
+            return ResponseEntity.status(400).body(new ApiResponse(errors.getFieldError().getDefaultMessage()));
+        }else {
+            userService.addUser(user);
+            return ResponseEntity.status(200).body(new ApiResponse("The user have been added successfully"));
+        }
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getUsers(){
+        List<User> users=userService.getUsers();
+        if (users.isEmpty()){
+            return ResponseEntity.status(400).body(new ApiResponse("There are no users to show"));
+        }else {
+            return ResponseEntity.status(200).body(users);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Integer id,@RequestBody @Valid User user, Errors errors){
+        if (errors.hasErrors()){
+            return ResponseEntity.status(400).body(new ApiResponse(errors.getFieldError().getDefaultMessage()));
+        }else {
+            if (userService.updateUser(id,user)){
+                return ResponseEntity.status(200).body(new ApiResponse("The user have been updated successfully"));
+            }else {
+                return ResponseEntity.status(400).body(new ApiResponse("There are no users with this id found"));
+            }
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Integer id){
+        if (userService.deleteUser(id)){
+            return ResponseEntity.status(200).body(new ApiResponse("The user have been deleted successfully"));
+        }else {
+            return ResponseEntity.status(400).body(new ApiResponse("There are no users with this id found"));
+        }
+    }
+
+
+}
